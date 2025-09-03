c175f3644f66b5063d934f78a5cce9ae05a9d578
ZobaerIslam <zobaerislam62@gmail.com>
2025-09-03 22:44:03 +0600

    feat: Improve UI and add button hover effect

--- app.py
+++ app.py
@@ -25,17 +25,40 @@ st.set_page_config(layout="wide")
 
 st.markdown("<h1 style='text-align: center;'>AI Resume Parser</h1>", unsafe_allow_html=True)
 
+st.markdown("""
+<style>
+.stTextArea label {font-size: 20px; font-weight: bold; color: maroon;}
+.stFileUploader label {font-size: 20px; font-weight: bold; color: maroon;}
+.stButton button {width: 100%; height: 50px;}
+.stButton button:hover {background-color: green;}
+.block-container {max-width: 1200px;}
+</style>
+""", unsafe_allow_html=True)
+
 col1, col2 = st.columns(2)
 
 with col1:
     uploaded_files = st.file_uploader("Upload your resumes", type=["pdf", "docx", "txt"], accept_multiple_files=True)
+    # if uploaded_files:
+    #     st.write("**Uploaded Files:**")
+    #     for file in uploaded_files:
+    #         st.write(file.name)
 
 with col2:
-    job_description = st.text_area("Enter the job description")
+    job_description = st.text_area("Enter the job description", height=300)
 
-# Button to trigger scoring
-if st.button("Get Score"):
-    if uploaded_files is not None and job_description is not None:
+
+_, col2, _ = st.columns([1, 1, 1])
+with col2:
+    get_score_button = st.button("Get Score")
+
+
+if get_score_button:
+    if not job_description:
+        st.warning("Please enter a job description.")
+    elif not uploaded_files:
+        st.warning("Please upload at least one resume.")
+    else:
         results = []
         for uploaded_file in uploaded_files:
             resume_text = read_resume_file(uploaded_file)
@@ -54,5 +77,4 @@ if st.button("Get Score"):
             st.dataframe(df)
             st.info("The matching score is calculated based on the semantic similarity between your resume and the job description. It measures how well the meaning of the text in your resume aligns with the meaning of the text in the job description.")
 
-    else:
-        st.error("Please upload at least one resume and enter a job description.")
+# st.info("Note: The file uploader in Streamlit has a known limitation where it only shows a few files at a time. To see all the uploaded files, please refer to the list above.")
